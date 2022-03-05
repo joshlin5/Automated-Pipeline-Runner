9987191a7b3deacbdab53e191f1db39b57889048
joshlin5 <linjos6750@hgmail.com>
2022-03-05 15:11:16 -0500

    fix setup part cannot run on M1.

--- commands/build.js
+++ commands/build.js
@@ -3,15 +3,21 @@ const chalk = require('chalk');
 const fs   = require('fs');
 const path = require('path');
 const yaml = require('js-yaml');
-const bakerxProvider = require('../lib/bakerxProvider');
+const sshExec = require('../lib/exec/ssh')
 const vmProvider = require("../lib/vmProvider");
 const buildFile = path.join( path.dirname(require.main.filename), 'build.yml')
 const doc = yaml.load(fs.readFileSync(buildFile, 'utf8'));
-const sshConfig = {
+const intelSshConfig = {
     host: '127.0.0.1',
     port: 2005,
     user: 'vagrant',
     identifyFile: path.join( path.dirname(require.main.filename), '.ssh', 'config-srv')
+};
+const vmSshConfig = {
+    host: '192.168.67.23',
+    port: 22,
+    user: 'ubuntu',
+    identifyFile: path.join( path.dirname(require.main.filename), 'keys', 'slim_rsa')
 };
 
 exports.command = 'build';
@@ -25,14 +31,20 @@ exports.builder = yargs => {
 exports.handler = async argv => {
     const { processor } = argv;
     console.log(chalk.green("triggering a build job"));
-    let provider = processor == "Intel/Amd64" ? bakerxProvider : vmProvider;
-    // console.log(doc.jobs[0].steps);
+    let config = null;
+    if(processor == "Intel/Amd64"){
+        config = intelSshConfig;
+    }else{
+        vmSshConfig['host'] = await vmProvider.getHost();
+        config = vmSshConfig;
+    }
     let mysql_pssw = new Map();
     mysql_pssw.set("{mysql_pssw}", process.env["mysql_pssw"])
+
     for(let i in doc.setup){
         let task = doc.setup[i];
         console.log(chalk.green(task.name));
-        await provider.ssh(task.cmd, sshConfig, mysql_pssw)
+        await sshExec(task.cmd, config, mysql_pssw)
     }
 
 };
