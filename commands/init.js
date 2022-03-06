2d4633798914425ad1e90d002594fa9188c10be4
gggho0806 <gggho0806@gmail.com>
2022-03-05 21:37:46 -0500

    change vmProvider to generate ssh key on M1

--- commands/init.js
+++ commands/init.js
@@ -1,7 +1,4 @@
 require('dotenv').config()
-const chalk = require('chalk');
-const path = require('path');
-const child = require('child_process');
 const bakerxProvider = require('../lib/bakerxProvider');
 const vmProvider = require("../lib/vmProvider");
 
@@ -18,7 +15,7 @@ exports.handler = async argv => {
     const {processor } = argv;
     console.log(chalk.green("Preparing computing environment..."));
     let provider = processor == "Intel/Amd64" ? bakerxProvider : vmProvider;
-
+    await provider.exec('mkdir -p ./.ssh');
     await provider.delete("M1")
     await provider.run();
 };
