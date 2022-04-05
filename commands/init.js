2f69140c2ef00c64e65b6d2486acd484e73e7334
gggho0806 <gggho0806@gmail.com>
2022-04-04 20:47:35 -0400

    change the VM's name to 'M2'

--- commands/init.js
+++ commands/init.js
@@ -14,7 +14,7 @@ exports.builder = yargs => {
 
 exports.handler = async argv => {
     const { processor } = argv;
-    const vm_name = 'M1';
+    const vm_name = 'M2';
     console.log(chalk.green("Preparing computing environment..."));
     let provider = processor == "Arm64" ? vmProvider : bakerxProvider;
     await provider.delete(vm_name);
